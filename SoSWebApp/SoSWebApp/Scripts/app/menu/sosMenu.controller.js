51dd4c73043d6635d6715d824b3486ac633fc114
Noah McCarn <nmccarn@elon.edu>
2017-06-29 15:55:48 -0400

    Added SoS Web Application

---
+++ SoSWebApp/SoSWebApp/Scripts/app/menu/sosMenu.controller.js
@@ -0,0 +1,5 @@
+﻿(function () {
+    angular.module('sos').controller('sosMenuController', ['$scope', function ($scope) {
+        $scope.test = 'menu test';
+    }]);
+})();
---
+++ SoSWebApp/SoSWebApp/Scripts/app/menu/sosMenu.directive.js
@@ -0,0 +1,10 @@
+﻿(function () {
+    angular.module('sos').directive('sosMenu', function () {
+        return {
+            restrict: 'E',
+            scope: {},
+            templateUrl: 'Scripts/app/menu/sosMenu.html',
+            controller: 'sosMenuController'
+        }
+    })
+})();
